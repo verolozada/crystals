47e494751ea8175c36fb3fd5dcbde9861cbede07
Veronica L <verolozada12@gmail.com>
2018-09-10 23:52:33 -0400

    font family changed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,9 @@
-// generate random numbers for the user to guess between 19-120
-// assign values to the cristal
-// click the crystals and show the value on screen
-// sum the values from the crystal clicked
-// if the number is equal to the computer wins increase by one and reset
-// if the number is above the computer's choice the losses increase by one and reset
-//create a function to reset the values
-
 // variables to keep score
 var wins = 0;
 var losses = 0;
+
+//images
+var images = ["assets/images/win.jpg","assets/images/Game_Over.jpg" ];
 
 //set of numbers  for the computer to choose
 var computerNumber = [];
@@ -41,6 +36,16 @@ function reset() {
     cnumber = 0;
     $("#cvalue").html("Your score is: " + cnumber);
 };
+
+function picture(src, height, width, alt) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.height = height;
+    img.width = width;
+    img.alt = alt;
+    
+    return document.getElementById("cvalue").append(img);
+  }
 
 function win() {
     wins++;
@@ -84,8 +89,10 @@ function count() {
         $("#cvalue").html("Your score is: " + cnumber);
 
         if (cnumber === number) {
+            // picture(images[0], 300, 200, win);
             win();    
         } else if (cnumber > number) {
+            // picture(images[1], 300, 200, gameOver);
             lose();
         } 
     });  
